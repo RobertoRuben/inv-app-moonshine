a424ee988657d4295006ddcd470f9f8b4cdcbb98
RobertoRuben <robertoch263@gmail.com>
2025-02-21 11:58:52 -0500

    ✨ feat(dashboard): integrate Apexcharts for asset metrics visualization

--- app/MoonShine/Pages/Dashboard.php
+++ app/MoonShine/Pages/Dashboard.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Pages;
 
+use App\Models\Asset;
+use App\Models\AssetCategory;
+use MoonShine\Apexcharts\Components\DonutChartMetric;
+use MoonShine\Apexcharts\Components\LineChartMetric;
 use MoonShine\Laravel\Pages\Page;
 use MoonShine\Contracts\UI\ComponentContract;
+use MoonShine\UI\Components\Card;
+use MoonShine\UI\Components\Heading;
+use MoonShine\UI\Components\Layout\Column;
+use MoonShine\UI\Components\Layout\Grid;
+use MoonShine\UI\Components\Layout\LineBreak;
+use MoonShine\UI\Components\Metrics\Wrapped\ValueMetric;
 
 class Dashboard extends Page
 {
@@ -29,6 +39,52 @@ class Dashboard extends Page
      */
     protected function components(): iterable
 	{
-		return [];
+        return [
+            Heading::make('Welcome to Inventory App', 1),
+            Heading::make('Dashboard', 2),
+            LineBreak::make(),
+
+            Grid::make([
+                Column::make([
+                    ValueMetric::make('Total Assets')
+                        ->value(Asset::count()),
+                ])->columnSpan(4),
+
+                Column::make([
+                    ValueMetric::make('Active Assets')
+                        ->value(Asset::where('status', true)->count()),
+                ])->columnSpan(4),
+
+                Column::make([
+                    ValueMetric::make('Inactive Assets')
+                        ->value(Asset::where('status', false)->count()),
+                ])->columnSpan(4),
+            ]) ,
+
+            LineBreak::make(),
+
+            Grid::make([
+                Column::make([
+                    DonutChartMetric::make('Assets by Category')
+                        ->values(function() {
+                            $data = AssetCategory::withCount('assets')->get();
+                            return $data->pluck('assets_count', 'name')->toArray();
+                        }),
+                ])->columnSpan(6),
+
+                Column::make([
+                    LineChartMetric::make('Assets Over Time')
+                        ->line(function() {
+                            $data = Asset::query()
+                                ->selectRaw("DATE(acquisition_date) as date, COUNT(*) as count")
+                                ->groupBy('date')
+                                ->orderBy('date')
+                                ->pluck('count', 'date')
+                                ->toArray();
+                            return ['Assets' => $data];
+                        }),
+                ])->columnSpan(6),
+            ]),
+        ];
 	}
 }
